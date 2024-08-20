beb7133804c8aed605a12a73788c20a4fe71de7b
oksanakaur <160805189+oksanakaur@users.noreply.github.com>
2024-08-20 08:09:24 +0200

    Updated html, css and javascript

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const container = document.querySelector('.container');
 const search = document.querySelector('.search-box button');
 const weatherBox = document.querySelector('weather-box');
 const weatherDetails = document.querySelector('.weather-details');
+const error404 = document.querySelector('.not-found');
 
 search.addEventListener('click', () => {
     const APIKey = 'd97c03e14d8eb31369107c48533d983b';
@@ -12,6 +13,20 @@ search.addEventListener('click', () => {
 
         fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${APIKey}`
         ).then(response => response.json()).then(json => {
+
+          if (json.cod == '404') {
+            container.style.height = '400px';
+            weatherBox.classList.remove('active');
+            weatherDetails.classList.remove('active');
+            error404.classList.add('active');
+            return;
+          }
+
+          container.style.height = "555px";
+          weatherBox.classList.add("active");
+          weatherDetails.classList.add("active");
+          error404.classList.remove("active");
+
         
             const image = document.querySelector('.weather-box img');
             const temperature = document.querySelector('.weather-box .temperature');
